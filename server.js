ea665980a8972d6475594aeec43f8e479e7333ed
seabassgonzalez <seabassgonzalez@gmail.com>
2017-01-13 12:46:20 -0800

    Add dependencies including morgan to handle request logs, body-parser for parsing JSON posts, specify local host index.html and bower     components locations

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 // Dependencies
-
+var express = require('express');
+var mongoose = require('mongoose');
+var port = process.env.PORT || 3000;
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
 // Express Config setting connection to MongoDB
 
 // Logging and Parsing
