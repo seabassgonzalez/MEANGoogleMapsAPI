e0d1e54efbc024c3fabe71e4521b17346672756c
seabassgonzalez <seabassgonzalez@gmail.com>
2017-01-13 12:53:05 -0800

    Commentary pseudocoding expected logging and parsing, routes, and listening sections

--- server.js
+++ server.js
@@ -6,10 +6,23 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var app = express();
+
 // Express Config setting connection to MongoDB
+mongoose.connect("mongodb://localhost/MEANGoogleMapsAPI");
 
 // Logging and Parsing
+// set static files location to public
+// specify use of bower components
+// log with morgan
+// parse application/json
+// parse application urlencoded
+// allow bodyParser to access raw texdt
+// parse application/vnd.api+json
+// specify use of override
 
 // Routes
+// require app routes.js
 
 // Listen
+// app listen(port)
+// console.log listening message
