588dbd3e8ef4bd79eff342e3459824adcc43492e
seabassgonzalez <seabassgonzalez@gmail.com>
2017-01-13 18:08:11 -0800

    Initial commentary for mongoose schema creating the user JSON structure, create pre-save logic setting created_at and updated_at, create mongodb collection of map-users to hold data

---
+++ app/model.js
@@ -0,0 +1,9 @@
+// Require mongoose and mongoose.schema
+
+// Create user schema
+
+// Set created_at to current time
+
+// Index this schema in 2dsphere for running proximity searches
+
+// Export UserSchema set MongoDB collection as map-users
