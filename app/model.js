ef63bb9ff9e516edc7912253f7b60f87e5382239
seabassgonzalez <seabassgonzalez@gmail.com>
2017-01-13 23:35:23 -0800

    Establish user schema for user json, username, location type, time appreciated, favorite feature, and whether the location is verified. Set presave logic for created_at and updated_at set to current time. Indexed UserSchema using 2dsphere for mongodb and mongoose to run geopsatial queries, set mongodb collection

--- app/model.js
+++ app/model.js
@@ -1,9 +1,31 @@
 // Require mongoose and mongoose.schema
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 // Create user schema
+var UserSchema = newSchema({
+  username: {type: String, required: true},
+  locationType: {type: String, required: true},
+  appreciationTime: {type: Number, required: true},
+  favfeature: {type: String, required: true},
+  location: {type: [Number], required: true},
+  htmlverified: String,
+  created_at: {type: Date, default: Date.now},
+  updated_at: {type: Date, default: Date.now}
+});
 
 // Set created_at to current time
+UserSchema.pre('save', function(next){
+    now = new Date();
+    this.updated_at = now;
+    if(!this.created_at) {
+        this.created_at = now
+    }
+    next();
+});
 
 // Index this schema in 2dsphere for running proximity searches
+UserSchema.index({location: '2dsphere'});
 
 // Export UserSchema set MongoDB collection as map-users
+module.exports = mongoose.model('scotch-user', UserSchema);
