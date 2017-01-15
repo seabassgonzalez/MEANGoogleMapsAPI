a7f1dc9523d86beaec7ac972b84b4014c345f85f
seabassgonzalez <seabassgonzalez@gmail.com>
2017-01-14 16:52:25 -0800

    Initial implementation of routes pseudocode, get and post routes

--- app/routes.js
+++ app/routes.js
@@ -16,3 +16,29 @@
         // send error
       // else
         // respond by placing json into body
+
+var mongoose = require('mongoose');
+var User = require('./model.js');
+
+module.exports = function(app){
+  app.get('/users', function(req, res){
+    var query = User.find({});
+    query.exec(function(err, users){
+      if(err){
+        res.send(err);
+      } else {
+        res.json(users);
+      }
+    });
+  });
+  app.post('/users', function(req, res){
+    var newuser = new User(req.body);
+    newuser.save(function(err){
+      if(err){
+        res.send(err);
+      } else {
+        res.json(req.body);
+      }
+    });
+  });
+};
