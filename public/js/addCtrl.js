ed615f1dafddcee94b2ff1eac1b8103b8f86042f
seabassgonzalez <seabassgonzalez@gmail.com>
2017-01-17 11:30:55 -0800

    Initial implementation of pseudocode for addCtrl.js in which the angular controller takes the values input by user for each textbox field, requests a post to the /users db to save the dta to userData, then clears the form

--- public/js/addCtrl.js
+++ public/js/addCtrl.js
@@ -7,6 +7,7 @@
   // Set initial coordinates $scope.formData.longitude
 
   // Functions
+
   // $scope.createUser method 
     // Create new user from form fields taking form text field values
       // username
@@ -15,7 +16,8 @@
       // favFeature
       // location
       // htmlverified
-    // .post method saves user data to database
+
+    // $http.post method saves user data to database
       // Empty form
         // username
         // locationType
@@ -23,3 +25,38 @@
         // favFeature
       // report error
         // console.logged
+
+var addCtrl = angular.module('addCtrl', ['geolocation']);
+addCtrl.controller('addCtrl', function($scope, $http, geolocation){
+
+  $scope.formData = {};
+  var coords = {};
+  var lat = 0;
+  var long = 0;
+
+  $scope.formData.latitude = 37.7952;
+  $scope.formData.longitude = 122.4028;
+
+  $scope.createUser = function(){
+
+    var userData = {
+      username: $scope.formData.username,
+      locationType: $scope.formData.locationType;
+      appreciationTime: $scope.formData.appreciationTime;
+      favfeature: $scope.formData.favfeature;
+      location: [$scope.formData.longitude, $scope.formData.latitude],
+      htmlverified: $scope.formData.htmlverified;
+    };
+
+    $http.post('/users', userData).success(function(data){
+      $scope.formData.username = "";
+      $scope.formData.locationType = "";
+      $scope.formData.appreciationTime = "";
+      $scope.formData.favfeature= "";
+    }).error(function(data){
+      console.log("There was an error with: " + data);
+    });
+  };
+});
+
+
