0925ea31c5306ac0e063a31274ee5c71e64ebbc5
seabassgonzalez <seabassgonzalez@gmail.com>
2017-01-17 11:17:22 -0800

    Initial addCtrl.js Angular Module pseudocode

---
+++ public/js/addCtrl.js
@@ -0,0 +1,25 @@
+// Instantiate addCtrl Angular Module and Controller using geolocation module and service
+// Instantiate addCtrl.controller takes 'addCtrl' '$scope' '$http' and 'geolocation'
+  
+  // Initialize variables formData, coords, lat, and long 
+  
+  // Set initial coordinates $scope.formData.latitude
+  // Set initial coordinates $scope.formData.longitude
+
+  // Functions
+  // $scope.createUser method 
+    // Create new user from form fields taking form text field values
+      // username
+      // locationType
+      // appreciationTime
+      // favFeature
+      // location
+      // htmlverified
+    // .post method saves user data to database
+      // Empty form
+        // username
+        // locationType
+        // appreciationTime
+        // favFeature
+      // report error
+        // console.logged
