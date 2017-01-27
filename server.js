8f296a419b93aa4ed7fcbb0b64775969d096d30a
seabassgonzalez <seabassgonzalez@gmail.com>
2017-01-27 06:19:53 -0800

    Mongoose default Promises mPromise deprecated, specify global.Promise for workaround

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 // Dependencies
 var express = require('express');
 var mongoose = require('mongoose');
+// Mongoose default promises deprecated, specify behavior to accomodate
+mongoose.Promise = global.Promise;
 var port = process.env.PORT || 3000;
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var app = express();
+  
 
+  
 // Express Config setting connection to MongoDB
 mongoose.connect("mongodb://localhost/MEANGoogleMapsAPI");
 
