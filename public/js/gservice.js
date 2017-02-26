6573febf44f57e3294fb8ce0174a015db7a58777
seabassgonzalez <seabassgonzalez@gmail.com>
2017-02-26 15:11:23 -0800

    Initial gservice pseudocode commentary, create gservice Angular module, initialize googleMapService object, locations array, selectedLat and selectedLong variables, refresh function within googleMapService

--- public/js/gservice.js
+++ public/js/gservice.js
@@ -1,19 +1,19 @@
 // Creates the gservice factory. This will be the primary means by which we interact with Google Maps
-        
+
         // Initialize Variables
         // -------------------------------------------------------------
         // Service our factory will return
 
         // Array of locations obtained from API calls
-        
+
         // Selected Location (initialize to center of America)
-        
+
         // Functions
         // --------------------------------------------------------------
         // Refresh the Map with new data. Function will take new latitude and longitude coordinates.
 
             // Clears the holding array of locations
-        
+
             // Set the selected lat and long equal to the ones provided on the refresh() call
 
             // Perform an AJAX call to get all of the records in the db.
@@ -23,13 +23,13 @@
         // Private Inner Functions
         // --------------------------------------------------------------
         // Convert a JSON of users into map points
-                
+
             // Clear the locations holder
 
             // Loop through all of the JSON entries provided in the response
 
                 // Create popup windows for each record
-            
+
                 // Converts each of the JSON records into Google Maps Location format (Note [Lat, Lng] format).
 
         // location is now an array populated with records in Google Maps format
@@ -37,11 +37,13 @@
 // Initializes the map
 
     // Uses the selected lat, long as starting point
-        
+
     // If map has not been created already...
 
         // Create a new map and place in the index.html page
-    
+
     // Loop through each location in the array and place a marker
-                
-  
+
+    // For each marker created, add a listener that checks for clicks
+
+        // When clicked, open the selected marker's message
