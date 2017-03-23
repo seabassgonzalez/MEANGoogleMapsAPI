5b9b7a098e784d3d41189d236220e0a4b7752763
seabassgonzalez <seabassgonzalez@gmail.com>
2017-03-23 10:28:45 -0700

    Get user's verified HTML based coordinates

--- public/js/addCtrl.js
+++ public/js/addCtrl.js
@@ -6,6 +6,11 @@
   // Set initial coordinates $scope.formData.latitude
   // Set initial coordinates $scope.formData.longitude
 
+  // Get User's actual coordinates based on HTML5 at window load
+    // Set the latitude and longitude equal to the HTML5 coordinates
+    // Display coordinates in location textboxes rounded to three decimal points
+    // Display message confirming that the coordinates verified.
+
   // Functions
 
   // Get coordinates based on click
@@ -40,14 +45,23 @@ addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, g
   $scope.formData.latitude = 37.7952;
   $scope.formData.longitude = 122.4028;
 
-  // Get coordinates based on mouse click. When a click event is detected....
-  $rootScope.$on("clicked", function(){
+geolocation.getLocation().then(function(data){
+    coords = {lat:data.coords.latitude, long:data.coords.longitude};
 
-    // Run the gservice functions associated with identifying coordinates
+    $scope.formData.longitude = parseFloat(coords.long).toFixed(3);
+    $scope.formData.latitude = parseFloat(coords.lat).toFixed(3);
+
+    $scope.formData.htmlverified = "Yes (Thank you for verifying)";
+
+    gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
+
+});
+
+  $rootScope.$on("clicked", function(){
     $scope.$apply(function(){
         $scope.formData.latitude = parseFloat(gservice.clickLat).toFixed(3);
         $scope.formData.longitude = parseFloat(gservice.clickLong).toFixed(3);
-        $scope.formData.htmlverified = "Nope (Thanks for spamming my map...)";
+        $scope.formData.htmlverified = "No";
     });
   });
 
